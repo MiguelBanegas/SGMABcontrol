025203f9bda995a60cd5ac04a372adfc7af6feaa
MiguelBanegas <miguelangelbanegas@gmail.com>
2025-12-29 11:47:59 -0300

    Fix database constraint to allow new roles in VPS

---
+++ server/migrations/20251229114500_fix_user_role_constraint.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.raw(
+    "ALTER TABLE users DROP CONSTRAINT IF EXISTS users_role_check"
+  );
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return Promise.resolve();
+};
