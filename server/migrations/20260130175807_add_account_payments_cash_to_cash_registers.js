db55abe5b4afb0017245ad0a5044478f31460035
MiguelBanegas <miguelangelbanegas@gmail.com>
2026-05-04 18:51:46 -0300

    fix: corrijo migraciones para que sean idempotentes

--- server/migrations/20260130175807_add_account_payments_cash_to_cash_registers.js
+++ server/migrations/20260130175807_add_account_payments_cash_to_cash_registers.js
@@ -1,7 +1,10 @@
-exports.up = function (knex) {
-  return knex.schema.table("cash_registers", (table) => {
-    table.decimal("account_payments_cash", 15, 2).defaultTo(0);
-  });
+exports.up = async function (knex) {
+  const hasColumn = await knex.schema.hasColumn("cash_registers", "account_payments_cash");
+  if (!hasColumn) {
+    return knex.schema.table("cash_registers", (table) => {
+      table.decimal("account_payments_cash", 15, 2).defaultTo(0);
+    });
+  }
 };
 
 exports.down = function (knex) {
--- server/migrations/20260130182719_add_inflow_column_to_cash_registers.js
+++ server/migrations/20260130182719_add_inflow_column_to_cash_registers.js
@@ -1,7 +1,10 @@
-exports.up = function (knex) {
-  return knex.schema.table("cash_registers", (table) => {
-    table.decimal("inflows", 15, 2).defaultTo(0);
-  });
+exports.up = async function (knex) {
+  const hasColumn = await knex.schema.hasColumn("cash_registers", "inflows");
+  if (!hasColumn) {
+    return knex.schema.table("cash_registers", (table) => {
+      table.decimal("inflows", 15, 2).defaultTo(0);
+    });
+  }
 };
 
 exports.down = function (knex) {
